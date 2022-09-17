76211a528ce3cdaabc00f87dceb98a69fcf8300d
orw4 <76228002+orw4@users.noreply.github.com>
2022-09-17 12:42:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # Visenere code
 # 17/9/2022
 
+def shift(l, num):
+    if l in letters:
+        i = letters.index(l)
+        i = (i + num) % len(letters)
+        l = letters[i]
+    return(l)
+
+def encrCaesar(message, num):
+    encrypted = ""
+    for i in message:
+        encrypted += shift(i,num)
+    return(encrypted)
+
+def decrCaesar(message,num):
+    
+
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+#print(encrCaesar("hello!",1))
